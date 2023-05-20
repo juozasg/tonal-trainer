0b9be680b656f597711460048ed32bc35019debd
Juozas Gaigalas <juozasgaigalas@gmail.com>
2023-05-21 00:06:50 +0300

    'cleanup'

--- mmidi/mmidi.rb
+++ mmidi/mmidi.rb
@@ -1,29 +1,23 @@
 # gem install micromidi
 # gem install unimidi
 
-
 require "unimidi"
 require "midi"
 
 # # prompt the user to select an input and output
 
-# @input = UniMIDI::Input.gets
-# @output = UniMIDI::Output.gets
+# input = UniMIDI::Input.gets
+# output = UniMIDI::Output.gets
 input = UniMIDI::Input.first.open
 output = UniMIDI::Output.first.open
 
-# pp @input
 
 puts "input = #{input.name}"
 puts "output = #{output.name}"
-# pp @output
 
 puts "playing..."
 
 m = MIDI::Session.new(output)
-# pp UniMIDI.constants
-# pp output.methods
-# exit
 
 
 #   1.times do |oct|
@@ -31,7 +25,6 @@ m.octave 3
 %w{C E G B}.each { |n| m.play n, 0.1 }
 #   end
 
-# pp input
 loop do
     puts "waiting for input..."
     n = input.gets
@@ -42,5 +35,4 @@ loop do
     m.play note, 1.6 if cc == 128
     # m.note(note)
     # m.off 
-
 end
